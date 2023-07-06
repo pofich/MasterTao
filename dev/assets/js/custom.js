481c5463c7308364271bf3cf5c9ed9995a82751e
pofich <firych23@gmail.com>
2023-07-06 14:30:08 +0300

    first screen mobile done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
     $('.first-screen-slider').slick({
         dots:true,
+        Infinite:true,
         appendArrows:".first-screen-slider_arrows",
+        responsive: [
+            {
+              breakpoint: 991,
+              settings: {
+               arrows:false,
+              }
+            },
+          ]
     });
   });
